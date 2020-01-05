722c9d34955b9089795a4860dc3ed73bf910c2cc
Cody Mathis <cody.mathis123@gmail.com>
2020-01-05 00:46:03 -0500

    convert to CIM

--- Public/Get-CCMLastHeartbeat.ps1
+++ Public/Get-CCMLastHeartbeat.ps1
@@ -7,52 +7,74 @@ function Get-CCMLastHeartbeat {
         The Discovery Data Collection Cycle major, and minor version is included. 
 
         This is also known as a 'Heartbeat' or 'DDR'
+    .PARAMETER CimSessions
+        Provides CimSessions to gather Discovery Data Collection Cycle last run info from
     .PARAMETER ComputerName
         Provides computer names to gather Discovery Data Collection Cycle last run info from
-    .PARAMETER Credential
-        Provides optional credentials to use for the WMI cmdlets.
     .EXAMPLE
-        C:\PS> Get-CCMLastHeartbeat 
+        C:\PS> Get-CCMLastHeartbeat
             Returns info regarding the last Discovery Data Collection Cycle for the local computer
     .EXAMPLE
-        C:\PS> Get-CCMLastHeartbeat -ComputerName 'Workstation1234','Workstation4321' 
+        C:\PS> Get-CCMLastHeartbeat -ComputerName 'Workstation1234','Workstation4321'
             Returns info regarding the last Discovery Data Collection Cycle for Workstation1234, and Workstation4321
     .NOTES
         FileName:    Get-CCMLastHeartbeat.ps1
         Author:      Cody Mathis
         Contact:     @CodyMathis123
         Created:     2019-01-01
-        Updated:     2019-01-01
+        Updated:     2019-01-05
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
     param (
-        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
-        [Alias('Computer', 'PSComputerName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
-        [string[]]$ComputerName = $env:COMPUTERNAME,
-        [parameter(Mandatory = $false)]
-        [pscredential]$Credential
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
+        [CimSession[]]$CimSession,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
+        [Alias('Connection', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
+        [string[]]$ComputerName = $env:ComputerName
     )
     begin {
-        $getWmiObjectLastHinv = @{
+        $getLastDDRSplat = @{
             Namespace = 'root\CCM\InvAgt'
             Query     = "SELECT LastCycleStartedDate, LastReportDate, LastMajorReportVersion, LastMinorReportVersion, InventoryActionID FROM InventoryActionStatus WHERE InventoryActionID = '{00000000-0000-0000-0000-000000000003}'"
         }
-        if ($PSBoundParameters.ContainsKey('Credential')) {
-            $getWmiObjectLastHinv['Credential'] = $Credential
-        }
     }
     process {
-        foreach ($Computer in $ComputerName) {
+        foreach ($Connection in (Get-Variable -Name $PSCmdlet.ParameterSetName -ValueOnly)) {
+            $Computer = switch ($PSCmdlet.ParameterSetName) {
+                'ComputerName' {
+                    Write-Output -InputObject $Connection
+                    switch ($Connection -eq $env:ComputerName) {
+                        $false {
+                            if ($ExistingCimSession = Get-CimSession -ComputerName $Connection -ErrorAction Ignore) {
+                                Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                                $getLastDDRSplat['CimSession'] = $ExistingCimSession
+                            }
+                            else {
+                                Write-Verbose "No active CimSession found for $Connection - falling back to -ComputerName parameter for CIM cmdlets"
+                                $getLastDDRSplat.Remove('CimSession')
+                                $getLastDDRSplat['ComputerName'] = $Connection
+                            }
+                        }
+                        $true {
+                            Write-Verbose 'Local computer is being queried - skipping computername, and cimsession parameter'
+                        }
+                    }
+                }
+                'CimSession' {
+                    Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                    Write-Output -InputObject $Connection.ComputerName
+                    $getLastDDRSplat[($PSCmdlet.ParameterSetName)] = $Connection
+                }
+            }
             $Result = [System.Collections.Specialized.OrderedDictionary]::new()
             $Result['ComputerName'] = $Computer
-            $getWmiObjectLastHinv['ComputerName'] = $Computer
 
             try {
-                [System.Management.ManagementObject[]]$LastHinv = Get-WmiObject @getWmiObjectLastHinv
-                if ($LastHinv -is [Object] -and $LastHinv.Count -gt 0) {
-                    foreach ($Occurrence in $LastHinv) {
-                        $Result['LastCycleStartedDate'] = [DateTime]::ParseExact(($Occurrence.LastCycleStartedDate.Split('+|-')[0]), 'yyyyMMddHHmmss.ffffff', [System.Globalization.CultureInfo]::InvariantCulture)
-                        $Result['LastReportDate'] = [DateTime]::ParseExact(($Occurrence.LastReportDate.Split('+|-')[0]), 'yyyyMMddHHmmss.ffffff', [System.Globalization.CultureInfo]::InvariantCulture)
+                [ciminstance[]]$LastDDR = Get-CimInstance @getLastDDRSplat
+                if ($LastDDR -is [Object] -and $LastDDR.Count -gt 0) {
+                    foreach ($Occurrence in $LastDDR) {
+                        $Result['LastCycleStartedDate'] = $Occurrence.LastCycleStartedDate
+                        $Result['LastReportDate'] = $Occurrence.LastReportDate
                         $Result['LastMajorReportVersion'] = $Occurrence.LastMajorReportVersion
                         $Result['LastMinorReportVersion'] = $Occurrence.LastMinorReportVersion
                         [PSCustomObject]$Result
