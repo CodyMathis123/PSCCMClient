f2fb561a2d3cbd65b3af28acbf8dc8bd3755146e
Cody Mathis <cody.mathis123@gmail.com>
2020-01-05 00:48:06 -0500

    convert to CIM

--- Public/Get-CCMLastSoftwareInventory.ps1
+++ Public/Get-CCMLastSoftwareInventory.ps1
@@ -1,65 +1,85 @@
 function Get-CCMLastSoftwareInventory {
     <#
     .SYNOPSIS
-        Returns info about the last time a heartbeat ran. Also known as a DDR.
+        Returns info about the last time Software Inventory ran
     .DESCRIPTION
-        This function will return info about the last time Software Inventory Cycle was ran. This is pulled from the InventoryActionStatus WMI Class. 
-        The Software Inventory Cycle major, and minor version is included. 
-
-        This is also known as a 'Heartbeat' or 'DDR'
+        This function will return info about the last time Software Inventory was ran. This is pulled from the InventoryActionStatus WMI Class.
+        The Software inventory major, and minor version is included. This can be helpful in troubleshooting Software inventory issues.
+    .PARAMETER CimSession
+        Provides CimSession to gather Software inventory last run info from
     .PARAMETER ComputerName
-        Provides computer names to gather Software Inventory Cycle last run info from
-    .PARAMETER Credential
-        Provides optional credentials to use for the WMI cmdlets.
+        Provides computer names to gather Software inventory last run info from
     .EXAMPLE
-        C:\PS> Get-CCMLastSoftwareInventory 
-            Returns info regarding the last Software Inventory Cycle for the local computer
+        C:\PS> Get-CCMLastSoftwareInventory
+            Returns info regarding the last Software inventory cycle for the local computer
     .EXAMPLE
-        C:\PS> Get-CCMLastSoftwareInventory -ComputerName 'Workstation1234','Workstation4321' 
-            Returns info regarding the last Software Inventory Cycle for Workstation1234, and Workstation4321
+        C:\PS> Get-CCMLastSoftwareInventory -ComputerName 'Workstation1234','Workstation4321'
+            Returns info regarding the last Software inventory cycle for Workstation1234, and Workstation4321
     .NOTES
         FileName:    Get-CCMLastSoftwareInventory.ps1
         Author:      Cody Mathis
         Contact:     @CodyMathis123
         Created:     2019-01-01
-        Updated:     2019-01-01
+        Updated:     2019-01-05
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
     param (
-        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
-        [Alias('Computer', 'PSComputerName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
-        [string[]]$ComputerName = $env:COMPUTERNAME,
-        [parameter(Mandatory = $false)]
-        [pscredential]$Credential
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
+        [CimSession[]]$CimSession,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
+        [Alias('Connection', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
+        [string[]]$ComputerName = $env:ComputerName
     )
     begin {
-        $getWmiObjectLastHinv = @{
+        $getLastSINVSplat = @{
             Namespace = 'root\CCM\InvAgt'
             Query     = "SELECT LastCycleStartedDate, LastReportDate, LastMajorReportVersion, LastMinorReportVersion, InventoryActionID FROM InventoryActionStatus WHERE InventoryActionID = '{00000000-0000-0000-0000-000000000002}'"
         }
-        if ($PSBoundParameters.ContainsKey('Credential')) {
-            $getWmiObjectLastHinv['Credential'] = $Credential
-        }
     }
     process {
-        foreach ($Computer in $ComputerName) {
+        foreach ($Connection in (Get-Variable -Name $PSCmdlet.ParameterSetName -ValueOnly)) {
+            $Computer = switch ($PSCmdlet.ParameterSetName) {
+                'ComputerName' {
+                    Write-Output -InputObject $Connection
+                    switch ($Connection -eq $env:ComputerName) {
+                        $false {
+                            if ($ExistingCimSession = Get-CimSession -ComputerName $Connection -ErrorAction Ignore) {
+                                Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                                $getLastSINVSplat['CimSession'] = $ExistingCimSession
+                            }
+                            else {
+                                Write-Verbose "No active CimSession found for $Connection - falling back to -ComputerName parameter for CIM cmdlets"
+                                $getLastSINVSplat.Remove('CimSession')
+                                $getLastSINVSplat['ComputerName'] = $Connection
+                            }
+                        }
+                        $true {
+                            Write-Verbose 'Local computer is being queried - skipping computername, and cimsession parameter'
+                        }
+                    }
+                }
+                'CimSession' {
+                    Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                    Write-Output -InputObject $Connection.ComputerName
+                    $getLastSINVSplat[($PSCmdlet.ParameterSetName)] = $Connection
+                }
+            }
             $Result = [System.Collections.Specialized.OrderedDictionary]::new()
             $Result['ComputerName'] = $Computer
-            $getWmiObjectLastHinv['ComputerName'] = $Computer
 
             try {
-                [System.Management.ManagementObject[]]$LastHinv = Get-WmiObject @getWmiObjectLastHinv
-                if ($LastHinv -is [Object] -and $LastHinv.Count -gt 0) {
-                    foreach ($Occurrence in $LastHinv) {
-                        $Result['LastCycleStartedDate'] = [DateTime]::ParseExact(($Occurrence.LastCycleStartedDate.Split('+|-')[0]), 'yyyyMMddHHmmss.ffffff', [System.Globalization.CultureInfo]::InvariantCulture)
-                        $Result['LastReportDate'] = [DateTime]::ParseExact(($Occurrence.LastReportDate.Split('+|-')[0]), 'yyyyMMddHHmmss.ffffff', [System.Globalization.CultureInfo]::InvariantCulture)
+                [ciminstance[]]$LastSINV = Get-CimInstance @getLastSINVSplat
+                if ($LastSINV -is [Object] -and $LastSINV.Count -gt 0) {
+                    foreach ($Occurrence in $LastSINV) {
+                        $Result['LastCycleStartedDate'] = $Occurrence.LastCycleStartedDate
+                        $Result['LastReportDate'] = $Occurrence.LastReportDate
                         $Result['LastMajorReportVersion'] = $Occurrence.LastMajorReportVersion
                         $Result['LastMinorReportVersion'] = $Occurrence.LastMinorReportVersion
                         [PSCustomObject]$Result
                     }
                 }
                 else {
-                    Write-Warning "No Software Inventory Cycle run found for $Computer"
+                    Write-Warning "No Software inventory run found for $Computer"
                 }
             }
             catch {
