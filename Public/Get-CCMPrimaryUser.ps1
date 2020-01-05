fb885339b97986a0f61d850639e2a3cb2f16b176
Cody Mathis <cody.mathis123@gmail.com>
2020-01-05 00:59:09 -0500

    convert to CIM

--- Public/Get-CCMPrimaryUser.ps1
+++ Public/Get-CCMPrimaryUser.ps1
@@ -2,64 +2,83 @@ function Get-CCMPrimaryUser {
     <#
     .SYNOPSIS
         Return primary users for a computer
-
     .DESCRIPTION
-        Pulls a list of primary users from WMI on the specified computer(s)
-
+        Pulls a list of primary users from WMI on the specified computer(s) or CIMSession(s)
+    .PARAMETER CimSession
+        Provides CimSession to gather primary users info from
+    .PARAMETER ComputerName
+        Provides computer names to gather primary users info from
     .EXAMPLE
         PS> Get-CCMPrimaryUser
-        Name                           Value
-        ----                           -----
-        Computer123                    contoso\codymathis123
-
-    .OUTPUTS
-        [System.Collections.Hashtable]
-
+            Returns all primary users listed in WMI on the local computer
     .NOTES
-        Returns a hashtable with the computername as the key, and the value is an array of usernames
-#>
+        FileName:    Get-CCMPrimaryUser.ps1
+        Author:      Cody Mathis
+        Contact:     @CodyMathis123
+        Created:     2019-01-05
+        Updated:     2019-01-05
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
     param (
-        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
-        [Alias('Computer', 'PSComputerName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
-        [string[]]$ComputerName = $env:COMPUTERNAME,
-        [parameter(Mandatory = $false)]
-        [PSCredential]$Credential
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
+        [CimSession[]]$CimSession,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
+        [Alias('Connection', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
+        [string[]]$ComputerName = $env:ComputerName
     )
     begin {
         #region define our hash tables for parameters to pass to Get-WMIObject and our return hash table
-        $GetWMI_Params = @{ }
-        switch ($true) {
-            $PSBoundParameters.ContainsKey('Credential') {
-                $GetWMI_Params['Credential'] = $Credential
-            }
+        $getPrimaryUserSplat = @{
+            NameSpace = 'root\CCM\CIModels'
+            Query     = 'SELECT User from CCM_PrimaryUser'
         }
-        $GetWMI_Params['Namespace'] = 'root\CCM\CIModels'
-        $GetWMI_Params['Query'] = "SELECT User from CCM_PrimaryUser"
         #endregion define our hash tables for parameters to pass to Get-WMIObject and our return hash table
     }
     process {
-        foreach ($Computer in $ComputerName) {
-            $Return = [System.Collections.Specialized.OrderedDictionary]::new()
-
-            try {
-                #region Query WMI for Primary User
-                $GetWMI_Params['ComputerName'] = $Computer
-                $WMI_Return = Get-WmiObject @GetWMI_Params
-                switch ($WMI_Return) {
-                    $null {
-                        Write-Warning "No primary users identified for $Computer"
-                    }
-                    default {
-                        $Return[$Computer] = $WMI_Return.User 
+        foreach ($Connection in (Get-Variable -Name $PSCmdlet.ParameterSetName -ValueOnly)) {
+            $Computer = switch ($PSCmdlet.ParameterSetName) {
+                'ComputerName' {
+                    Write-Output -InputObject $Connection
+                    switch ($Connection -eq $env:ComputerName) {
+                        $false {
+                            if ($ExistingCimSession = Get-CimSession -ComputerName $Connection -ErrorAction Ignore) {
+                                Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                                $getPrimaryUserSplat['CimSession'] = $ExistingCimSession
+                            }
+                            else {
+                                Write-Verbose "No active CimSession found for $Connection - falling back to -ComputerName parameter for CIM cmdlets"
+                                $getPrimaryUserSplat.Remove('CimSession')
+                                $getPrimaryUserSplat['ComputerName'] = $Connection
+                            }
+                        }
+                        $true {
+                            Write-Verbose 'Local computer is being queried - skipping computername, and cimsession parameter'
+                        }
                     }
                 }
-                #endregion Query WMI for Primary User
+                'CimSession' {
+                    Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                    Write-Output -InputObject $Connection.ComputerName
+                    $getPrimaryUserSplat[($PSCmdlet.ParameterSetName)] = $Connection
+                }
+            }
+            $Result = [System.Collections.Specialized.OrderedDictionary]::new()
+            $Result['ComputerName'] = $Computer
+
+            try {
+                [ciminstance[]]$PrimaryUsers = Get-CimInstance @getPrimaryUserSplat
+                if ($PrimaryUsers -is [Object] -and $PrimaryUsers.Count -gt 0) {
+                    $Result['PrimaryUser'] = $PrimaryUsers.User
+                    [PSCustomObject]$Result
+                }
+                else {
+                    Write-Warning "No Primary Users found for $Computer"
+                }
             }
             catch {
-                Write-Error "Failed to establed WMI Connection to $Computer"
+                $ErrorMessage = $_.Exception.Message
+                Write-Error $ErrorMessage
             }
-
-            Write-Output $Return
         }
     }
 }
