dad2174985548698264347c49d53270fd109fa05
Cody Mathis <cody.mathis123@gmail.com>
2020-01-05 22:54:16 -0500

    convert to CIM

--- Public/Get-CCMServiceWindow.ps1
+++ Public/Get-CCMServiceWindow.ps1
@@ -1,17 +1,15 @@
 function Get-CCMServiceWindow {
     <#
     .SYNOPSIS
-        Get ConfigMgr Service Window information from computers via WMI
+        Get ConfigMgr Service Window information from computers via CIM
     .DESCRIPTION
-        This function will allow you to gather Service Window information from multiple computers using WMI queries. Note that 'ServiceWindows' are object
+        This function will allow you to gather Service Window information from multiple computers using CIM queries. Note that 'ServiceWindows' are object
         that describe the schedule for a maintenance window, such as the recurrence, and date / time information. You can provide an array of computer names,
         or you can pass them through the pipeline. You are also able to specify the Service Window Type (SWType) you wish to query for, and pass credentials.
         What is returned is the data from the 'ActualConfig' section of WMI on the computer. The data returned will include the 'schedules' as well as
         the schedule type. Note that the schedules are not really 'human readable' and can be passed into ConvertFrom-CCMSchedule to convert
         them into a readable object. This is the equivalent of the 'Convert-CMSchedule' cmdlet that is part of the SCCM PowerShell module, but
         it does not require the module and it is much faster.
-    .PARAMETER ComputerName
-        Provides computer names to gather SW info from.
     .PARAMETER SWType
         Specifies the types of SW you want information for. Valid options are below
             'All Deployment Service Window',
@@ -20,8 +18,10 @@ function Get-CCMServiceWindow {
             'Software Update Service Window',
             'Task Sequences Service Window',
             'Corresponds to non-working hours'
-    .PARAMETER Credential
-        Provides optional credentials to use for the WMI cmdlets.
+    .PARAMETER CimSession
+        Provides CimSessions to gather Service Window information info from
+    .PARAMETER ComputerName
+        Provides computer names to gather Service Window information info from
     .EXAMPLE
         C:\PS> Get-CCMSchedule
             Return all the 'All Deployment Service Window', 'Software Update Service Window' Maintenance Windows for the local computer. These are the two default MW types
@@ -36,11 +36,8 @@ function Get-CCMServiceWindow {
         Created:     2019-12-12
         Updated:     2019-12-31
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
     param (
-        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
-        [Alias('Computer', 'PSComputerName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
-        [string[]]$ComputerName = $env:COMPUTERNAME,
         [parameter(Mandatory = $false)]
         [ValidateSet('All Deployment Service Window',
             'Program Service Window',
@@ -50,8 +47,11 @@ function Get-CCMServiceWindow {
             'Corresponds to non-working hours')]
         [Alias('MWType')]
         [string[]]$SWType = @('All Deployment Service Window', 'Software Update Service Window'),
-        [parameter(Mandatory = $false)]
-        [pscredential]$Credential
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
+        [CimSession[]]$CimSession,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
+        [Alias('Connection', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
+        [string[]]$ComputerName = $env:ComputerName
     )
     begin {
         #region Create hashtable for mapping MW types, and create WMI filter based on input params
@@ -64,24 +64,49 @@ function Get-CCMServiceWindow {
             6	=	'Corresponds to non-working hours'
         }
 
-        $RequestedTypesRaw = $SW_Type.Keys.Where( { $SW_Type[$_] -in $SW_Type } )
+        $RequestedTypesRaw = $SW_Type.Keys.Where( { $SW_Type[$_] -in $SWType } )
 
         $RequestedTypesFilter = [string]::Format('ServiceWindowType = {0}', [string]::Join(' OR ServiceWindowType =', $RequestedTypesRaw))
         #endregion Create hashtable for mapping MW types, and create WMI filter based on input params
-        $getWmiObjectServiceWindowSplat = @{
+        $getServiceWindowSplat = @{
             Namespace = 'root\CCM\Policy\Machine\ActualConfig'
-            Class     = 'CCM_ServiceWindow'
+            ClassName = 'CCM_ServiceWindow'
             Filter    = $RequestedTypesFilter
         }
     }
     process {
-        foreach ($Computer in $ComputerName) {
+        foreach ($Connection in (Get-Variable -Name $PSCmdlet.ParameterSetName -ValueOnly)) {
+            $Computer = switch ($PSCmdlet.ParameterSetName) {
+                'ComputerName' {
+                    Write-Output -InputObject $Connection
+                    switch ($Connection -eq $env:ComputerName) {
+                        $false {
+                            if ($ExistingCimSession = Get-CimSession -ComputerName $Connection -ErrorAction Ignore) {
+                                Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                                $getServiceWindowSplat['CimSession'] = $ExistingCimSession
+                            }
+                            else {
+                                Write-Verbose "No active CimSession found for $Connection - falling back to -ComputerName parameter for CIM cmdlets"
+                                $getServiceWindowSplat.Remove('CimSession')
+                                $getServiceWindowSplat['ComputerName'] = $Connection
+                            }
+                        }
+                        $true {
+                            Write-Verbose 'Local computer is being queried - skipping computername, and cimsession parameter'
+                        }
+                    }
+                }
+                'CimSession' {
+                    Write-Verbose "Active CimSession found for $Connection - Passing CimSession to CIM cmdlets"
+                    Write-Output -InputObject $Connection.ComputerName
+                    $getServiceWindowSplat[($PSCmdlet.ParameterSetName)] = $Connection
+                }
+            }
             $Result = [System.Collections.Specialized.OrderedDictionary]::new()
             $Result['ComputerName'] = $Computer
-            $getWmiObjectServiceWindowSplat['ComputerName'] = $Computer
 
             try {
-                [System.Management.ManagementObject[]]$ServiceWindows = Get-WmiObject @getWmiObjectServiceWindowSplat
+                [ciminstance[]]$ServiceWindows = Get-CimInstance @getServiceWindowSplat
                 if ($ServiceWindows -is [Object] -and $ServiceWindows.Count -gt 0) {
                     foreach ($ServiceWindow in $ServiceWindows) {
                         $Result['Schedules'] = $ServiceWindow.Schedules
