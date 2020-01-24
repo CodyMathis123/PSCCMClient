f32c69769ea36a40f4658d7eee84b33fc46aa9a3
Cody Mathis <cody.mathis123@gmail.com>
2020-01-23 23:16:52 -0500

    adding help, whatif, pipeline

--- Public/Invoke-CCMApplication.ps1
+++ Public/Invoke-CCMApplication.ps1
@@ -1,18 +1,64 @@
 # TODO - Finish writing function
 
 Function Invoke-CCMApplication {
+    <#
+    .SYNOPSIS
+        Invoke the provided method for an application deployed to a computer
+    .DESCRIPTION
+        Uses the Install, or Uninstall method of the CCM_Application CIMClass to perform actions on applications.
+    .PARAMETER ID
+        An array of ID to invoke
+    .PARAMETER IsMachineTarget
+        Boolean value that specifies if the application is machine targeted, or user targeted
+    .PARAMETER Revision
+        The revision of the application that will have an action invoked. This is needed so that MEMCM knows
+            what policy it should be working with.
+    .PARAMETER Method
+        Install, or Uninstall. Keep in mind that you can only perform whatever action is available for an application.
+            If it is a required application that does not allow uninstall, then the invoke will not work.
+    .PARAMETER EnforcePreference
+        When the install should take place. Options are 'Immediate', 'NonBusinessHours', or 'AdminSchedule'
 
+        Defaults to 'Immediate'
+    .PARAMETER Priority
+        The priority that is passed to the method. Options are 'Foreground', 'High', 'Normal', and 'Low'
+
+        Defaults to 'High'
+    .PARAMETER IsRebootIfNeeded
+        Boolean that tells MEMCM if it can reboot the computer IF a reboot is required after the method completes based on exit code.
+    .PARAMETER CimSession
+        Provides CimSession to invoke the application method on
+    .PARAMETER ComputerName
+        Provides computer names to invoke the application method on
+    .EXAMPLE
+        PS> Invoke-CCMApplication
+            Returns all deployed applications listed in WMI on the local computer
+    .EXAMPLE
+        PS> Invoke-CCMApplication -ID ScopeId_BE389CA5-D6CC-42AF-B8F5-A059F9C9AD91/Application_0607d288-fc0b-42b7-9a61-76abedf0673e
+    .NOTES
+        FileName:    Invoke-CCMApplication.ps1
+        Author:      Cody Mathis
+        Contact:     @CodyMathis123
+        Created:     2020-01-21
+        Updated:     2020-01-23
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'ComputerName')]
     Param
     (
-        [Parameter(Mandatory = $false)]
-        [string[]]$ApplicationName,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ID,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [bool[]]$IsMachineTarget,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string[]]$Revision,
         [Parameter(Mandatory = $true)]
-        [ValidateSet('Install', 'Uninstall', 'DownloadContents')]
+        [ValidateSet('Install', 'Uninstall')]
         [string]$Method,
         [Parameter(Mandatory = $false)]
         [ValidateSet('Immediate', 'NonBusinessHours', 'AdminSchedule')]
         [string]$EnforcePreference = 'Immediate',
         [Parameter(Mandatory = $false)]
+        [ValidateSet('Foreground', 'High', 'Normal', 'Low')]
         [string]$Priority = 'High',
         [Parameter(Mandatory = $false)]
         [bool]$IsRebootIfNeeded = $false,
@@ -23,21 +69,20 @@ Function Invoke-CCMApplication {
         [string[]]$ComputerName = $env:ComputerName
     )
     begin {
-        $Application = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computername | Where-Object { $_.Name -like $AppName })
         $connectionSplat = @{ }
         $EnforcePreferenceMap = @{
             'Immediate'        = [uint32]0
             'NonBusinessHours' = [uint32]1
-            'AdminSchedule'    = [uint32]2        
+            'AdminSchedule'    = [uint32]2
         }
         $invokeAppMethodSplat = @{
             NameSpace  = 'root\CCM\ClientSDK'
             ClassName  = 'CCM_Application'
             MethodName = $Method
             Arguments  = @{
+                Priority          = $Priority
                 EnforcePreference = $EnforcePreferenceMap[$EnforcePreference]
                 IsRebootIfNeeded  = $IsRebootIfNeeded
-                Priority          = $Priority
             }
         }
     }
@@ -73,12 +118,28 @@ Function Invoke-CCMApplication {
                     $connectionSplat['CimSession'] = $Connection
                 }
             }
+            $Result = [System.Collections.Specialized.OrderedDictionary]::new()
+            $Result['ComputerName'] = $Computer
+            $Result['AppMethodInvoked'] = $false
 
-            foreach ($AppToInvoke in $NeedToFigureOut) {
-                $invokeAppMethodArgs.Arguments['ID'] = $AppToInvoke.ID
-                $invokeAppMethodSplat.Arguments['Revision'] = $AppToInvoke.Revision
-                $invokeAppMethodSplat.Arguments['IsMachineTarget'] = $AppToInvoke.IsMachineTarget
-                Invoke-CimMethod  -MethodName $Method -Arguments $Args
+            foreach ($AppID in $ID) {
+                if ($PSCmdlet.ShouldProcess("[Method = '$Method'] [ID = '$AppID'] [ComputerName = '$Computer']", "Invoke-CCMApplication")) {
+                    $invokeAppMethodSplat.Arguments['ID'] = [string]$AppID
+                    $invokeAppMethodSplat.Arguments['Revision'] = [string]$Revision
+                    $invokeAppMethodSplat.Arguments['IsMachineTarget'] = [bool]$IsMachineTarget
+                    try {
+                        $Invocation = Invoke-CimMethod @invokeAppMethodSplat @connectionSplat
+                        switch ($Invocation.ReturnValue) {
+                            0 {
+                                $Result['AppMethodInvoked'] = $true
+                            }
+                        }
+                    }
+                    catch {
+                        Write-Error "Failed to invoke [Method = '$Method'] [ID = '$AppID'] [ComputerName = '$Computer'] - $($_.Exception.Message)"
+                    }
+                    [pscustomobject]$Result
+                }
             }
         }
     }
