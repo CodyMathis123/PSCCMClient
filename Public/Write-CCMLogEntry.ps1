2d1e70d782e1b05a4bc3bc9d70e6290693912bfd
Cody Mathis <cody.mathis123@gmail.com>
2020-01-23 22:01:33 -0500

    update help, add pipeline support, code cleanup

--- Public/Write-CCMLogEntry.ps1
+++ Public/Write-CCMLogEntry.ps1
@@ -1,113 +1,138 @@
 Function Write-CCMLogEntry {
     <#
-    .DESCRIPTION
-        Write CMTrace friendly log files with options for log rotation
-    .EXAMPLE
-        $Bias = (Get-CimInstance -ClassName Win32_TimeZone).Bias
-        $FileName = "myscript_" + (Get-Date -Format 'yyyy-MM-dd_HH-mm-ss') + ".log"
-        Write-CCMLogEntry -Value "Writing text to log file" -Severity 1 -Component "Some component name" -FileName $FileName -Folder "C:\Windows\temp" -Bias $Bias -Enable -MaxLogFileSize 1MB -MaxNumOfRotatedLogs 3
+        .SYNOPSIS
+            Write to a log file in the CMTrace Format
+        .DESCRIPTION
+            The function is used to write to a log file in a CMTrace compatible format. This ensures that CMTrace or OneTrace can parse the log
+            and provide data in a familiar format.
+        .PARAMETER Value
+            String to be added it to the log file as the message, or value
+        .PARAMETER Severity
+            Severity for the log entry. 1 for Informational, 2 for Warning, and 3 for Error.
+        .PARAMETER Component
+            Stage that the log entry is occuring in, log refers to as 'component.'
+        .PARAMETER FileName
+            Name of the log file that the entry will written to - note this should not be the full path.
+        .PARAMETER Folder
+            Path to the folder where the log will be stored.
+        .PARAMETER Bias
+            Set timezone Bias to ensure timestamps are accurate. This defaults to the local machines bias, but one can be provided. It can be
+            helperful to gather the bias once, and store it in a variable that is passed to this parameter as part of a splat, or $PSDefaultParameterValues
+        .PARAMETER MaxLogFileSize
+            Maximum size of log file before it rolls over. Set to 0 to disable log rotation. Defaults to 5MB
+        .PARAMETER LogsToKeep
+            Maximum number of rotated log files to keep. Set to 0 for unlimited rotated log files. Defaults to 0.
+        .EXAMPLE
+            C:\PS> Write-CCMLogEntry -Value 'Testing Function' -Component 'Test Script' -FileName 'LogTest.Log' -Folder 'c:\temp'
+                Write out 'Testing Function' to the c:\temp\LogTest.Log file in a CMTrace format, noting 'Test Script' as the component.
+        .NOTES
+            FileName:    Get-CCMSite.ps1
+            Author:      Cody Mathis
+            Contact:     @CodyMathis123
+            Created:     2020-01-23
+            Updated:     2020-01-23
     #>
     param (
-        [parameter(Mandatory = $true, HelpMessage = 'Value added to the log file.')]
-        [ValidateNotNullOrEmpty()]
-        [string]$Value,
-        [parameter(Mandatory = $false, HelpMessage = 'Severity for the log entry. 1 for Informational, 2 for Warning and 3 for Error.')]
-        [ValidateNotNullOrEmpty()]
-        [ValidateSet('1', '2', '3')]
-        [string]$Severity = 1,
-        [parameter(Mandatory = $false, HelpMessage = "Stage that the log entry is occuring in, log refers to as 'component'.")]
-        [ValidateNotNullOrEmpty()]
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias('Message', 'ToLog')]
+        [string[]]$Value,
+        [parameter(Mandatory = $false)]
+        [ValidateSet(1, 2, 3)]
+        [int]$Severity = 1,
+        [parameter(Mandatory = $false)]
         [string]$Component,
-        [parameter(Mandatory = $true, HelpMessage = 'Name of the log file that the entry will written to.')]
-        [ValidateNotNullOrEmpty()]
+        [parameter(Mandatory = $true)]
         [string]$FileName,
-        [parameter(Mandatory = $true, HelpMessage = 'Path to the folder where the log will be stored.')]
-        [ValidateNotNullOrEmpty()]
+        [parameter(Mandatory = $true)]
         [string]$Folder,
-        [parameter(Mandatory = $false, HelpMessage = 'Set timezone Bias to ensure timestamps are accurate.')]
-        [ValidateNotNullOrEmpty()]
-        [int32]$Bias,
-        [parameter(Mandatory = $false, HelpMessage = 'Maximum size of log file before it rolls over. Set to 0 to disable log rotation.')]
-        [ValidateNotNullOrEmpty()]
-        [int32]$MaxLogFileSize = 5MB,
-        [parameter(Mandatory = $false, HelpMessage = 'Maximum number of rotated log files to keep. Set to 0 for unlimited rotated log files.')]
-        [ValidateNotNullOrEmpty()]
-        [int32]$MaxNumOfRotatedLogs = 0,
-        [parameter(Mandatory = $false, HelpMessage = 'A switch that enables the use of this function.')]
-        [ValidateNotNullOrEmpty()]
-        [switch]$Enable
+        [parameter(Mandatory = $false)]
+        [int]$Bias = (Get-CimInstance -Query "SELECT Bias FROM Win32_TimeZone").Bias,
+        [parameter(Mandatory = $false)]
+        [int]$MaxLogFileSize = 5MB,
+        [parameter(Mandatory = $false)]
+        [int]$LogsToKeep = 0
     )
-    If ($Enable) {
+    begin {
         # Determine log file location
         $LogFilePath = Join-Path -Path $Folder -ChildPath $FileName
 
-        If ((([System.IO.FileInfo]$LogFilePath).Exists) -And ($MaxLogFileSize -ne 0)) {
+        #region log rollover check if $MaxLogFileSize is greater than 0
+        switch (([System.IO.FileInfo]$LogFilePath).Exists -and $MaxLogFileSize -gt 0) {
+            $true {
+                #region rename current file if $MaxLogFileSize exceeded, respecting $LogsToKeep
+                switch (([System.IO.FileInfo]$LogFilePath).Length -ge $MaxLogFileSize) {
+                    $true {
+                        # Get log file name without extension
+                        $LogFileNameWithoutExt = $FileName -replace ([System.IO.Path]::GetExtension($FileName))
 
-            # Get log size in bytes
-            $LogFileSize = ([System.IO.FileInfo]$LogFilePath).Length
+                        # Get already rolled over logs
+                        $AllLogs = Get-ChildItem -Path $Folder -Name "$($LogFileNameWithoutExt)_*" -File
 
-            If ($LogFileSize -ge $MaxLogFileSize) {
+                        # Sort them numerically (so the oldest is first in the list)
+                        $AllLogs = Sort-Object -InputObject $AllLogs -Descending { $_ -replace '_\d+\.lo_$' }, { [int]($_ -replace '^.+\d_|\.lo_$') } -ErrorAction Ignore
 
-                # Get log file name without extension
-                $LogFileNameWithoutExt = $FileName -replace ([System.IO.Path]::GetExtension($FileName))
-
-                # Get already rolled over logs
-                $AllLogs = Get-ChildItem -Path $Folder -Name "$($LogFileNameWithoutExt)_*" -File
-
-                # Sort them numerically (so the oldest is first in the list)
-                $AllLogs = Sort-Object -InputObject $AllLogs -Descending { $_ -replace '_\d+\.lo_$' }, { [Int]($_ -replace '^.+\d_|\.lo_$') } -ErrorAction Ignore
-            
-                ForEach ($Log in $AllLogs) {
-                    # Get log number
-                    $LogFileNumber = [int32][Regex]::Matches($Log, "_([0-9]+)\.lo_$").Groups[1].Value
-                    switch (($LogFileNumber -eq $MaxNumOfRotatedLogs) -And ($MaxNumOfRotatedLogs -ne 0)) {
-                        $true {
-                            # Delete log if it breaches $MaxNumOfRotatedLogs parameter value
-                            [System.IO.File]::Delete("$($Folder)\$($Log)")
+                        ForEach ($Log in $AllLogs) {
+                            # Get log number
+                            $LogFileNumber = [int][Regex]::Matches($Log, "_([0-9]+)\.lo_$").Groups[1].Value
+                            switch (($LogFileNumber -eq $LogsToKeep) -and ($LogsToKeep -ne 0)) {
+                                $true {
+                                    # Delete log if it breaches $LogsToKeep parameter value
+                                    [System.IO.File]::Delete("$($Folder)\$($Log)")
+                                }
+                                $false {
+                                    # Rename log to +1
+                                    $NewFileName = $Log -replace "_([0-9]+)\.lo_$", "_$($LogFileNumber+1).lo_"
+                                    [System.IO.File]::Copy("$($Folder)\$($Log)", "$($Folder)\$($NewFileName)", $true)
+                                }
+                            }
                         }
-                        $false {
-                            # Rename log to +1
-                            $NewFileName = $Log -replace "_([0-9]+)\.lo_$", "_$($LogFileNumber+1).lo_"
-                            [System.IO.File]::Copy("$($Folder)\$($Log)", "$($Folder)\$($NewFileName)", $true)
-                        }
+
+                        # Copy main log to _1.lo_
+                        [System.IO.File]::Copy($LogFilePath, "$($Folder)\$($LogFileNameWithoutExt)_1.lo_", $true)
+
+                        # Blank the main log
+                        $StreamWriter = New-Object -TypeName System.IO.StreamWriter -ArgumentList $LogFilePath, $false
+                        $StreamWriter.Close()
                     }
                 }
-
-                # Copy main log to _1.lo_
-                [System.IO.File]::Copy($LogFilePath, "$($Folder)\$($LogFileNameWithoutExt)_1.lo_", $true)
-
-                # Blank the main log
-                $StreamWriter = New-Object -TypeName System.IO.StreamWriter -ArgumentList $LogFilePath, $false
-                $StreamWriter.Close()
+                #endregion rename current file if $MaxLogFileSize exceeded, respecting $LogsToKeep
             }
         }
+        #endregion log rollover check if $MaxLogFileSize is greater than 0
 
-        # Construct time stamp for log entry
-        switch -regex ($Bias) {
-            '-' {
-                $Time = [string]::Concat($(Get-Date -Format 'HH:mm:ss.fff'), $Bias)
-            }
-            Default {
-                $Time = [string]::Concat($(Get-Date -Format 'HH:mm:ss.fff'), '+', $Bias)
-            }
-        }
         # Construct date for log entry
         $Date = (Get-Date -Format 'MM-dd-yyyy')
-    
+
         # Construct context for log entry
         $Context = $([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
-    
-        # Construct final log entry
-        $LogText = [string]::Format('<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="{4}" type="{5}" thread="{6}" file="">', $Value, $Time, $Date, $Component, $Context, $Severity, $PID)
-    
-        # Add value to log file
-        try {
-            $StreamWriter = New-Object -TypeName System.IO.StreamWriter -ArgumentList $LogFilePath, 'Append'
-            $StreamWriter.WriteLine($LogText)
-            $StreamWriter.Close()
-        }
-        catch [System.Exception] {
-            Write-Warning -Message "Unable to append log entry to $FileName file. Error message: $($_.Exception.Message)"
+    }
+    process {
+        foreach ($MSG in $Value) {
+            #region construct time stamp for log entry based on $Bias and current time
+            $Time = switch -regex ($Bias) {
+                '-' {
+                    [string]::Concat($(Get-Date -Format 'HH:mm:ss.fff'), $Bias)
+                }
+                Default {
+                    [string]::Concat($(Get-Date -Format 'HH:mm:ss.fff'), '+', $Bias)
+                }
+            }
+            #endregion construct time stamp for log entry based on $Bias and current time
+
+            #region construct the log entry according to CMTrace format
+            $LogText = [string]::Format('<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="{4}" type="{5}" thread="{6}" file="">', $MSG, $Time, $Date, $Component, $Context, $Severity, $PID)
+            #endregion construct the log entry according to CMTrace format
+
+            #region add value to log file
+            try {
+                $StreamWriter = New-Object -TypeName System.IO.StreamWriter -ArgumentList $LogFilePath, 'Append'
+                $StreamWriter.WriteLine($LogText)
+                $StreamWriter.Close()
+            }
+            catch [System.Exception] {
+                Write-Warning -Message "Unable to append log entry to $FileName file. Error message: $($_.Exception.Message)"
+            }
+            #endregion add value to log file
         }
     }
 }
