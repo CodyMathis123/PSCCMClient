25db87cdff1b93427006e9bfcf3564b1c0bdc921
Cody Mathis <cody.mathis123@gmail.com>
2020-01-26 22:12:10 -0500

    import function, add param

--- Public/Test-CCMStaleLog.ps1
+++ Public/Test-CCMStaleLog.ps1
@@ -16,6 +16,14 @@ Function Test-CCMStaleLog {
         Number of days of inactivity that you would consider the specified log stale.
     .PARAMETER MinutesStale
         Number of minutes of inactivity that you would consider the specified log stale.
+    .PARAMETER DisableCCMSetupFallback
+        Disable the CCMSetup fallback check - details below.
+
+        When the desired log file is not found, then the last modified timestamp for the CCMSetup log is checked.
+        When the CCMSetup file has activity within the last 24 hours, then we assume that, even though our desired
+        log file was not found, it isn't stale because the MEMCM client is recently installed or repaired.
+        If the CCMSetup is found, and has no activity, or is just not found, then we assume the desired
+        log is 'stale.' This additional chack can be disabled with this switch parameter.
     .EXAMPLE
         C:\PS> Test-CCMStaleLog -LogFileName ccmexec -DaysStale 2
             Check if the ccmexec log file has been written to within the last 2 days on the local computer
@@ -40,6 +48,8 @@ Function Test-CCMStaleLog {
         [int]$HoursStale,
         [Parameter(Mandatory = $false)]
         [int]$MinutesStale,
+        [Parameter(Mandatory = $false)]
+        [switch]$DisableCCMSetupFallback,
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
         [Microsoft.Management.Infrastructure.CimSession[]]$CimSession,
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
@@ -108,33 +118,44 @@ Function Test-CCMStaleLog {
             $Result = [System.Collections.Specialized.OrderedDictionary]::new()
             $Result['ComputerName'] = $Computer
             $Result['LogFileName'] = $LogFileName
+            $Result['LogLastWriteTime'] = $null
+            $Result['LogStale'] = $null
+            $Result['CCMSetupLastWriteTime'] = $null
             $CCMLogDirectory = (Get-CCMLoggingConfiguration @connectionSplat).LogDirectory
             $LogFullPath = [string]::Join('\', $CCMLogDirectory, $LogFileName)
 
             Write-Verbose $([string]::Format('Checking {0} for activity', $LogFullPath))
 
             $getRequestedLogInfoSplat['Query'] = [string]::Format('SELECT Readable, LastModified FROM CIM_DataFile WHERE Name = "{0}"', ($LogFullPath -replace "\\", "\\"))
+            # 'Poke' the log by querying it once. Log files sometimes do not show an accurate LastModified time until they are accessed
+            $null = Get-CimInstance @getRequestedLogInfoSplat @connectionSplat
             $RequestedLogInfo = Get-CimInstance @getRequestedLogInfoSplat @connectionSplat
+
+            $getRequestedLogInfoSplat['Query'] = [string]::Format('SELECT Readable, LastModified FROM CIM_DataFile WHERE Name = "{0}"', ($MEMCMClientInstallLog -replace "\\", "\\"))
+            # 'Poke' the log by querying it once. Log files sometimes do not show an accurate LastModified time until they are accessed
+            $null = Get-CimInstance @getRequestedLogInfoSplat @connectionSplat
+            $MEMCMClientInstallLogInfo = Get-CimInstance @getRequestedLogInfoSplat @connectionSplat
+            
+            if ($null -ne $MEMCMClientInstallLogInfo) {
+                $Result['CCMSetupLastWriteTime'] = ([datetime]$dtmMEMCMClientInstallLogDate = $MEMCMClientInstallLogInfo.LastModified)
+            }
             if ($null -ne $RequestedLogInfo) {
-                [datetime]$LogLastWriteTime = $RequestedLogInfo.LastModified
+                $Result['LogLastWriteTime'] = ([datetime]$LogLastWriteTime = $RequestedLogInfo.LastModified)
                 $LastWriteDiff = New-TimeSpan -Start $LogLastWriteTime -End (Get-Date -format yyyy-MM-dd)
                 if ($LastWriteDiff -gt $StaleTimeframe) {
-                    Write-Verbose $([string]::Format('{0} is not active', $LogFileName))
-                    Write-Verbose $([string]::Format('{0} last date modified is {1}', $LogFileName, $LogDate))
+                    Write-Verbose $([string]::Format('{0} is not active', $LogFullPath))
+                    Write-Verbose $([string]::Format('{0} last date modified is {1}', $LogFullPath, $LogDate))
                     Write-Verbose $([string]::Format("Current Date and Time is {0}", (Get-Date)))
                     $LogStale = $true
                 }
                 else {
-                    Write-Verbose $([string]::Format('{0}.log is active', $LogFileName))
+                    Write-Verbose $([string]::Format('{0}.log is active', $LogFullPath))
                     $LogStale = $false
                 }
             }
-            else {
-                Write-Warning $([string]::Format('{0} is missing; checking for recent ccmsetup activity', $LogFileName))
-                $getRequestedLogInfoSplat['Query'] = [string]::Format('SELECT Readable, LastModified FROM CIM_DataFile WHERE Name = "{0}"', ($MEMCMClientInstallLog -replace "\\", "\\"))
-                $MEMCMClientInstallLogInfo = Get-CimInstance @getRequestedLogInfoSplat @connectionSplat
+            elseif (-not $DisableCCMSetupFallback.IsPresent) {
+                Write-Warning $([string]::Format('{0} not found; checking for recent ccmsetup activity', $LogFullPath))
                 if ($null -ne $MEMCMClientInstallLogInfo) {
-                    [datetime]$dtmMEMCMClientInstallLogDate = $MEMCMClientInstallLogInfo.LastModified
                     [int]$ClientInstallHours = (New-TimeSpan -Start $dtmMEMCMClientInstallLogDate -End (Get-Date)).TotalHours
                     if ($ClientInstallHours -lt 24) {
                         Write-Warning 'CCMSetup activity detected within last 24 hours - marking log as not stale'
@@ -149,6 +170,10 @@ Function Test-CCMStaleLog {
                     Write-Warning $([string]::Format('CCMSetup.log not found in {0} - marking log as stale', $MEMCMClientInstallLog))
                     $LogStale = $true
                 }
+            }
+            else {
+                Write-Warning $([string]::Format('{0} not found', $LogFullPath))
+                $LogStale = 'File Not Found'
             }
             $Result['LogStale'] = $LogStale
             [pscustomobject]$Result
