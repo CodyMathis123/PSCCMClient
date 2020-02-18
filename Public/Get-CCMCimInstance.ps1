2770012bfdfcaef4d74226423035a748759b7299
Cody Mathis <cody.mathis123@gmail.com>
2020-02-18 07:12:26 -0500

    initial commit

---
+++ Public/Get-CCMCimInstance.ps1
@@ -0,0 +1,86 @@
+﻿# TODO - Add Help
+function Get-CCMCimInstance {
+	[CmdletBinding(DefaultParameterSetName = 'CimQuery-ComputerName')]
+	param
+	(
+		[Parameter(Mandatory = $false)]
+		[string]$Namespace,
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimFilter-CimSession')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimFilter-PSSession')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimFilter-ComputerName')]
+		[string]$ClassName,
+		[Parameter(Mandatory = $false, ParameterSetName = 'CimFilter-CimSession')]
+		[Parameter(Mandatory = $false, ParameterSetName = 'CimFilter-PSSession')]
+		[Parameter(Mandatory = $false, ParameterSetName = 'CimFilter-ComputerName')]
+		[string]$Filter,
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimQuery-CimSession')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimQuery-PSSession')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimQuery-ComputerName')]
+		[string]$Query,
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimQuery-ComputerName')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'CimFilter-ComputerName')]
+		[ValidateSet('CimSession', 'PSSession')]
+		[string]$ConnectionPreference = 'CimSession',
+		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimQuery-CimSession')]
+		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimFilter-CimSession')]
+		[Alias('CimQuery-CimSession', 'CimFilter-CimSession')]
+		[Microsoft.Management.Infrastructure.CimSession[]]$CimSession,
+		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimQuery-ComputerName')]
+		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimFilter-ComputerName')]
+		[Alias('Connection', 'PSComputerName', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
+		[string[]]$ComputerName = $env:ComputerName,
+		[Parameter(Mandatory = $false, ParameterSetName = 'CimQuery-PSSession')]
+		[Parameter(Mandatory = $false, ParameterSetName = 'CimFilter-PSSession')]
+		[Alias('Session', 'CimQuery-PSSession', 'CimFilter-PSSession')]
+		[System.Management.Automation.Runspaces.PSSession[]]$PSSession
+	)
+	begin {
+		$ConnectionChecker = switch -regex ($PSCmdlet.ParameterSetName) {
+			'ComputerName$' {
+				$ConnectionPreference
+			}
+			default {
+				($PSCmdlet.ParameterSetName).Split('-')[1]
+			}
+		}
+		$GetCimInstanceSplat = @{ }
+		$StringArgs = switch ($PSBoundParameters.Keys) {
+			'Namespace' {
+				$GetCimInstanceSplat['NameSpace'] = $Namespace
+				[string]::Format('-NameSpace "{0}"', $Namespace)
+			}
+			'ClassName' {
+				$GetCimInstanceSplat['ClassName'] = $ClassName
+				[string]::Format('-ClassName "{0}"', $ClassName)
+			}
+			'Filter' {
+				$GetCimInstanceSplat['Filter'] = $Filter
+				[string]::Format('-Filter "{0}"', $($Filter -replace '"','\"'))
+			}
+			'Query' {
+				$GetCimInstanceSplat['Query'] = $Query
+				[string]::Format('-Query "{0}"', $($Query -replace '"','\"'))
+			}
+		}
+	}
+	process {
+		foreach ($Connection in (Get-Variable -Name $ConnectionChecker -ValueOnly -Scope Local)) {
+			$getConnectionInfoSplat = @{
+				$PSCmdlet.ParameterSetName = $Connection
+			}
+			$ConnectionInfo = Get-CCMConnection @getConnectionInfoSplat -Prefer $ConnectionChecker
+			$connectionSplat = $ConnectionInfo.connectionSplat
+
+			switch ($ConnectionChecker) {
+				'CimSession' {
+					Get-CimInstance @GetCimInstanceSplat @connectionSplat
+				}
+				'PSSession' {
+					$ScriptBlockString = [string]::Format('Get-CimInstance {0}', ([string]::Join(' ', $StringArgs)))
+					$ScriptBlock = [scriptblock]::Create($ScriptBlockString)
+					Invoke-Command -ScriptBlock $ScriptBlock @connectionSplat
+				}
+			}
+		}
+	}
+}
