6ca0754252416e3902adcad0b4e468ecc0810941
Cody Mathis <CCM5521A@humana.com>
2020-02-24 13:37:16 -0500

    add PSSession support

--- Public/Set-CCMCacheLocation.ps1
+++ Public/Set-CCMCacheLocation.ps1
@@ -1,36 +1,45 @@
-# TODO - Add PSSession Support
 function Set-CCMCacheLocation {
     <#
-    .SYNOPSIS
-        Set ConfigMgr cache location from computers via CIM
-    .DESCRIPTION
-        This function will allow you to set the configuration manager cache location for multiple computers using CIM queries. 
-        You can provide an array of computer names, or cimsession, or you can pass them through the pipeline.
-        It will return a hashtable with the computer as key and boolean as value for success
-    .PARAMETER Location
-        Provides the desired cache location - note that ccmcache is appended if not provided as the end of the path
-    .PARAMETER CimSession
-        Provides CimSessions to set the cache location for
-    .PARAMETER ComputerName
-        Provides computer names to set the cache location for
-    .EXAMPLE
-        C:\PS> Set-CCMCacheLocation -Location d:\windows\ccmcache
-            Set cache location to d:\windows\ccmcache for local computer
-    .EXAMPLE
-        C:\PS> Set-CCMCacheLocation -ComputerName 'Workstation1234','Workstation4321' -Location 'C:\windows\ccmcache'
-            Set Cache location to 'C:\Windows\CCMCache' for Workstation1234, and Workstation4321
-    .EXAMPLE
-        C:\PS> Set-CCMCacheLocation -ComputerName 'Workstation1234','Workstation4321' -Location 'C:\temp\ccmcache'
-            Set Cache location to 'C:\temp\CCMCache' for Workstation1234, and Workstation4321
-    .EXAMPLE
-        C:\PS> Set-CCMCacheLocation -Location 'D:'
-            Set Cache location to 'D:\CCMCache' for the local computer
-    .NOTES
-        FileName:    Set-CCMCacheLocation.ps1
-        Author:      Cody Mathis
-        Contact:     @CodyMathis123
-        Created:     2019-11-06
-        Updated:     2020-01-09
+        .SYNOPSIS
+            Set ConfigMgr cache location from computers via CIM
+        .DESCRIPTION
+            This function will allow you to set the configuration manager cache location for multiple computers using CIM queries. 
+            You can provide an array of computer names, or cimsession, or you can pass them through the pipeline.
+            It will return a hashtable with the computer as key and boolean as value for success
+        .PARAMETER Location
+            Provides the desired cache location - note that ccmcache is appended if not provided as the end of the path
+        .PARAMETER CimSession
+            Provides CimSessions to set the cache location for
+        .PARAMETER ComputerName
+            Provides computer names to set the cache location for
+        .PARAMETER PSSession
+            Provides PSSessions to set the cache location for
+        .PARAMETER ConnectionPreference
+            Determines if the 'Get-CCMConnection' function should check for a PSSession, or a CIMSession first when a ComputerName
+            is passed to the funtion. This is ultimately going to result in the function running faster. The typicaly usecase is
+            when you are using the pipeline. In the pipeline scenario, the 'ComputerName' parameter is what is passed along the 
+            pipeline. The 'Get-CCMConnection' function is used to find the available connections, falling back from the preference
+            specified in this parameter, to the the alternative (eg. you specify, PSSession, it falls back to CIMSession), and then 
+            falling back to ComputerName. Keep in mind that the 'ConnectionPreference' also determines what type of connection / command
+            the ComputerName paramter is passed to. 
+        .EXAMPLE
+            C:\PS> Set-CCMCacheLocation -Location d:\windows\ccmcache
+                Set cache location to d:\windows\ccmcache for local computer
+        .EXAMPLE
+            C:\PS> Set-CCMCacheLocation -ComputerName 'Workstation1234','Workstation4321' -Location 'C:\windows\ccmcache'
+                Set Cache location to 'C:\Windows\CCMCache' for Workstation1234, and Workstation4321
+        .EXAMPLE
+            C:\PS> Set-CCMCacheLocation -ComputerName 'Workstation1234','Workstation4321' -Location 'C:\temp\ccmcache'
+                Set Cache location to 'C:\temp\CCMCache' for Workstation1234, and Workstation4321
+        .EXAMPLE
+            C:\PS> Set-CCMCacheLocation -Location 'D:'
+                Set Cache location to 'D:\CCMCache' for the local computer
+        .NOTES
+            FileName:    Set-CCMCacheLocation.ps1
+            Author:      Cody Mathis
+            Contact:     @CodyMathis123
+            Created:     2019-11-06
+            Updated:     2020-02-24
     #>
     [CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'ComputerName')]
     param (
@@ -41,7 +50,13 @@ function Set-CCMCacheLocation {
         [Microsoft.Management.Infrastructure.CimSession[]]$CimSession,
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
         [Alias('Connection', 'PSComputerName', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
-        [string[]]$ComputerName = $env:ComputerName
+        [string[]]$ComputerName = $env:ComputerName,
+        [Parameter(Mandatory = $false, ParameterSetName = 'PSSession')]
+        [Alias('Session')]
+        [System.Management.Automation.Runspaces.PSSession[]]$PSSession,
+        [Parameter(Mandatory = $false, ParameterSetName = 'ComputerName')]
+        [ValidateSet('CimSession', 'PSSession')]
+        [string]$ConnectionPreference
     )
     begin {       
         $FullCachePath = switch ($Location.EndsWith('ccmcache', 'CurrentCultureIgnoreCase')) {
@@ -67,14 +82,20 @@ function Set-CCMCacheLocation {
             $getConnectionInfoSplat = @{
                 $PSCmdlet.ParameterSetName = $Connection
             }
+            switch ($PSBoundParameters.ContainsKey('ConnectionPreference')) {
+                $true {
+                    $getConnectionInfoSplat['Prefer'] = $ConnectionPreference
+                }
+            }
             $ConnectionInfo = Get-CCMConnection @getConnectionInfoSplat
             $Computer = $ConnectionInfo.ComputerName
             $connectionSplat = $ConnectionInfo.connectionSplat
+
             $Return = [ordered]@{ }
 
             try {
                 if ($PSCmdlet.ShouldProcess("[ComputerName = '$Computer'] [Location = '$Location']", "Set CCM Cache Location")) {
-                    $Cache = Get-CimInstance @GetCacheSplat @connectionSplat
+                    $Cache = Get-CCMCimInstance @GetCacheSplat @connectionSplat
                     if ($Cache -is [object]) {
                         switch ($Cache.Location) {
                             $FullCachePath {
@@ -86,10 +107,10 @@ function Set-CCMCacheLocation {
                                         . $SetCacheScriptblock
                                     }
                                     $false {
-                                        Invoke-CIMPowerShell @SetCacheSplat @connectionSplat
+                                        Invoke-CCMCommand @SetCacheSplat @connectionSplat
                                     }
                                 }
-                                $Cache = Get-CimInstance @GetCacheSplat @connectionSplat
+                                $Cache = Get-CCMCimInstance @GetCacheSplat @connectionSplat
                                 switch ($Cache.Location) {
                                     $FullCachePath {
                                         $Return[$Computer] = $true
