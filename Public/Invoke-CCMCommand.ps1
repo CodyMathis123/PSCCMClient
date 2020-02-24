be4d8dc2eefd5dc664d4a95e76105027cf2b9261
Cody Mathis <CCM5521A@humana.com>
2020-02-24 10:05:53 -0500

    add help

--- Public/Invoke-CCMCommand.ps1
+++ Public/Invoke-CCMCommand.ps1
@@ -1,5 +1,54 @@
-﻿# TODO - Add Help
-function Invoke-CCMCommand {
+﻿function Invoke-CCMCommand {
+	<#
+		.SYNOPSIS
+			Invoke commands remotely via Win32_Process:CreateProcess, or Invoke-Command
+		.DESCRIPTION
+			This function is used as part of the PSCCMClient Module. It's purpose is to allow commands
+			to be execute remotely, while also automatically determining the best, or preferred method
+			of invoking the command. Based on the type of connection that is passed, whether a CimSession,
+			PSSession, or Computername with a ConnectionPreference, the command will be executed remotely
+			by either using the CreateProcess Method of the Win32_Process CIM Class, or it will use
+			Invoke-Command.
+		.PARAMETER ScriptBlock
+			The ScriptBlock that should be executed remotely
+		.PARAMETER FunctionsToLoad
+			A list of functions to load into the remote command exeuction. For example, you could specify that you want 
+			to load "Get-CustomThing" into the remote command, as you've already written the function and want to use
+			it as part of the scriptblock that will be remotely executed.
+		.PARAMETER Timeout
+			The time in milliseconds after which the NamedPipe will timeout. The NamedPipe connection is used in the 
+			CimSession parameter set, as this is how the object is returned from the remote command. 
+        .PARAMETER CimSession
+            Provides CimSessions to invoke the specified scriptblock on
+        .PARAMETER ComputerName
+            Provides computer names to invoke the specified scriptblock on
+        .PARAMETER PSSession
+            Provides PSSessions to invoke the specified scriptblock on
+        .PARAMETER ConnectionPreference
+            Determines if the 'Get-CCMConnection' function should check for a PSSession, or a CIMSession first when a ComputerName
+            is passed to the funtion. This is ultimately going to result in the function running faster. The typicaly usecase is
+            when you are using the pipeline. In the pipeline scenario, the 'ComputerName' parameter is what is passed along the
+            pipeline. The 'Get-CCMConnection' function is used to find the available connections, falling back from the preference
+            specified in this parameter, to the the alternative (eg. you specify, PSSession, it falls back to CIMSession), and then
+            falling back to ComputerName. Keep in mind that the 'ConnectionPreference' also determines what type of connection / command
+            the ComputerName paramter is passed to.
+		.EXAMPLE
+			C:\PS> Invoke-CCMCommand -ScriptBlock { 'Testing This' } -ComputerName Workstation123
+				Would return the string 'Testing This' which was executed on the remote machine Workstation123
+		.EXAMPLE
+			C:\PS> function Test-This {
+				'Testing This'
+			}
+			Invoke-CCMCommand -Scriptblock { Test-This } -FunctionsToLoad Test-This -ComputerName Workstation123
+				Would load the custom Test-This function into the scriptblock, and execute it. This would return the 'Testing This'
+				string, based on the function being executed remotely on Workstation123.
+		.NOTES
+			FileName:    Invoke-CCMCommand.ps1
+			Author:      Cody Mathis
+			Contact:     @CodyMathis123
+			Created:     2020-02-12
+			Updated:     2020-02-24
+	#>
 	[CmdletBinding(DefaultParameterSetName = 'ComputerName')]
 	param
 	(
@@ -10,9 +59,6 @@ function Invoke-CCMCommand {
 		[Parameter(Mandatory = $false, ParameterSetName = 'CimSession')]
 		[ValidateRange(1000, 900000)]
 		[int32]$Timeout = 120000,
-		[Parameter(Mandatory = $false, ParameterSetName = 'ComputerName')]
-		[ValidateSet('CimSession', 'PSSession')]
-		[string]$ConnectionPreference = 'CimSession',
 		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
 		[Microsoft.Management.Infrastructure.CimSession[]]$CimSession,
 		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
@@ -20,7 +66,10 @@ function Invoke-CCMCommand {
 		[string[]]$ComputerName = $env:ComputerName,
 		[Parameter(Mandatory = $false, ParameterSetName = 'PSSession')]
 		[Alias('Session')]
-		[System.Management.Automation.Runspaces.PSSession[]]$PSSession
+		[System.Management.Automation.Runspaces.PSSession[]]$PSSession,
+		[Parameter(Mandatory = $false, ParameterSetName = 'ComputerName')]
+		[ValidateSet('CimSession', 'PSSession')]
+		[string]$ConnectionPreference = 'CimSession'
 	)
 	begin {
 		$HelperFunctions = switch ($PSBoundParameters.ContainsKey('FunctionsToLoad')) {
