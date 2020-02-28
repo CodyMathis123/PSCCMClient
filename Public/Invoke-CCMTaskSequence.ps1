1dd32a389f65dc55da39de33fb76ebc7467930a6
Cody Mathis <cody.mathis123@gmail.com>
2020-02-27 23:34:31 -0500

    add PSSession support

--- Public/Invoke-CCMTaskSequence.ps1
+++ Public/Invoke-CCMTaskSequence.ps1
@@ -1,42 +1,51 @@
-# TODO - Add PSSession Support
 function Invoke-CCMTaskSequence {
     <#
-    .SYNOPSIS
-        Invoke deployed task sequence on a computer
-    .DESCRIPTION
-        This function can invoke a task sequence that is deployed to a computer. It has an optional 'Force' parameter which will
-        temporarily change the RepeatRunBehavioar, and MandatoryAssignments parameters to force a task sequence to run regardless
-        of the schedule and settings assigned to it.
+        .SYNOPSIS
+            Invoke deployed task sequence on a computer
+        .DESCRIPTION
+            This function can invoke a task sequence that is deployed to a computer. It has an optional 'Force' parameter which will
+            temporarily change the RepeatRunBehavioar, and MandatoryAssignments parameters to force a task sequence to run regardless
+            of the schedule and settings assigned to it.
 
-        Note that the parameters for filter are all joined together with OR.
-    .PARAMETER PackageID
-        An array of PackageID to filter on
-    .PARAMETER TaskSequenceName
-        An array of task sequence names to filter on
-    .PARAMETER Force
-        Force the task sequence to run by temporarily changing the RepeatRunBehavioar, and MandatoryAssignments parameters as shown below
+            Note that the parameters for filter are all joined together with OR.
+        .PARAMETER PackageID
+            An array of PackageID to filter on
+        .PARAMETER TaskSequenceName
+            An array of task sequence names to filter on
+        .PARAMETER Force
+            Force the task sequence to run by temporarily changing the RepeatRunBehavioar, and MandatoryAssignments parameters as shown below
 
-            Property = @{
-                ADV_RepeatRunBehavior    = 'RerunAlways'
-                ADV_MandatoryAssignments = $true
-            }
-    .PARAMETER CimSession
-        Provides CimSession to invoke the task sequence on
-    .PARAMETER ComputerName
-        Provides computer names to invoke the task sequence on
-    .EXAMPLE
-        PS> Invoke-CCMTaskSequence
-            Invoke all task sequence listed in WMI on the local computer
-    .EXAMPLE
-        PS> Invoke-CCMTaskSequence -TaskSequenceName 'Windows 10' -PackageID 'TST00443'
-            Invoke the deployed task sequence listed in WMI on the local computer which have either a task sequence name of 'Windows 10' or
-            a PackageID of 'TST00443'
-    .NOTES
-        FileName:    Invoke-CCMTaskSequence.ps1
-        Author:      Cody Mathis
-        Contact:     @CodyMathis123
-        Created:     2020-01-15
-        Updated:     2020-01-15
+                Property = @{
+                    ADV_RepeatRunBehavior    = 'RerunAlways'
+                    ADV_MandatoryAssignments = $true
+                }
+        .PARAMETER CimSession
+            Provides CimSession to invoke the task sequence on
+        .PARAMETER ComputerName
+            Provides computer names to invoke the task sequence on
+        .PARAMETER PSSession
+            Provides PSSessions to invoke the task sequence on
+        .PARAMETER ConnectionPreference
+            Determines if the 'Get-CCMConnection' function should check for a PSSession, or a CIMSession first when a ComputerName
+            is passed to the funtion. This is ultimately going to result in the function running faster. The typicaly usecase is
+            when you are using the pipeline. In the pipeline scenario, the 'ComputerName' parameter is what is passed along the
+            pipeline. The 'Get-CCMConnection' function is used to find the available connections, falling back from the preference
+            specified in this parameter, to the the alternative (eg. you specify, PSSession, it falls back to CIMSession), and then
+            falling back to ComputerName. Keep in mind that the 'ConnectionPreference' also determines what type of connection / command
+            the ComputerName parameter is passed to.
+        .EXAMPLE
+            PS> Invoke-CCMTaskSequence
+                Invoke all task sequence listed in WMI on the local computer
+        .EXAMPLE
+            PS> Invoke-CCMTaskSequence -TaskSequenceName 'Windows 10' -PackageID 'TST00443'
+                Invoke the deployed task sequence listed in WMI on the local computer which have either a task sequence name of 'Windows 10' or
+                a PackageID of 'TST00443'
+        .NOTES
+            FileName:    Invoke-CCMTaskSequence.ps1
+            Author:      Cody Mathis
+            Contact:     @CodyMathis123
+            Created:     2020-01-15
+            Updated:     2020-02-27
     #>
     [CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'ComputerName')]
     param (
@@ -52,7 +61,12 @@ function Invoke-CCMTaskSequence {
         [Microsoft.Management.Infrastructure.CimSession[]]$CimSession,
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
         [Alias('Connection', 'PSComputerName', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
-        [string[]]$ComputerName = $env:ComputerName
+        [string[]]$ComputerName = $env:ComputerName,
+        [Parameter(Mandatory = $false, ParameterSetName = 'PSSession')]
+        [System.Management.Automation.Runspaces.PSSession[]]$PSSession,
+        [Parameter(Mandatory = $false, ParameterSetName = 'ComputerName')]
+        [ValidateSet('CimSession', 'PSSession')]
+        [string]$ConnectionPreference
     )
     Begin {
         $setAlwaysRerunSplat = @{
@@ -63,7 +77,7 @@ function Invoke-CCMTaskSequence {
         }
 
         #region define our hash tables for parameters to pass to Get-CIMInstance and our return hash table
-        $getPackageSplat = @{
+        $getTaskSequenceSplat = @{
             NameSpace = 'root\CCM\Policy\Machine\ActualConfig'
         }
         #endregion define our hash tables for parameters to pass to Get-CIMInstance and our return hash table
@@ -83,9 +97,15 @@ function Invoke-CCMTaskSequence {
             $getConnectionInfoSplat = @{
                 $PSCmdlet.ParameterSetName = $Connection
             }
+            switch ($PSBoundParameters.ContainsKey('ConnectionPreference')) {
+                $true {
+                    $getConnectionInfoSplat['Prefer'] = $ConnectionPreference
+                }
+            }
             $ConnectionInfo = Get-CCMConnection @getConnectionInfoSplat
             $Computer = $ConnectionInfo.ComputerName
             $connectionSplat = $ConnectionInfo.connectionSplat
+
             $Result = [ordered]@{ }
             $Result['ComputerName'] = $Computer
 
@@ -107,20 +127,50 @@ function Invoke-CCMTaskSequence {
                             ' '
                         }
                     }
-                    $getPackageSplat['Query'] = [string]::Format('SELECT * FROM CCM_TaskSequence{0}', $Filter)
+                    $getTaskSequenceSplat['Query'] = [string]::Format('SELECT * FROM CCM_TaskSequence{0}', $Filter)
 
-                    [ciminstance[]]$TaskSequences = Get-CimInstance @getPackageSplat @connectionSplat
+                    [ciminstance[]]$TaskSequences = switch ($Computer -eq $env:ComputerName) {
+                        $true {
+                            Get-CimInstance @getTaskSequenceSplat @connectionSplat
+                        }
+                        $false {
+                            Get-CCMCimInstance @getTaskSequenceSplat @connectionSplat
+                        }
+                    }
                     if ($TaskSequences -is [Object] -and $TaskSequences.Count -gt 0) {
                         foreach ($Advertisement in $TaskSequences) {
                             switch ($Force.IsPresent) {
                                 $true {
                                     Write-Verbose "Force parameter present - Setting package to always rerun"
                                     $setAlwaysRerunSplat['InputObject'] = $Advertisement
-                                    Set-CimInstance @setAlwaysRerunSplat @connectionSplat
+                                    switch -regex ($ConnectionInfo.ConnectionType) {
+                                        '^ComputerName$|^CimSession$' {
+                                            Set-CimInstance @setAlwaysRerunSplat @connectionSplat
+                                        }
+                                        'PSSession' {
+                                            $invokeCommandSplat = @{
+                                                ScriptBlock  = {
+                                                    param (
+                                                        $setAlwaysRerunSplat
+                                                    )
+                                                    Set-CimInstance @setAlwaysRerunSplat
+                                                }
+                                                ArgumentList = $setAlwaysRerunSplat
+                                            }
+                                            Invoke-Command @invokeCommandSplat @connectionSplat
+                                        }
+                                    }
                                 }
                             }
-                            $getPackageSplat['Query'] = [string]::Format("SELECT ScheduledMessageID FROM CCM_Scheduler_ScheduledMessage WHERE ScheduledMessageID LIKE '{0}%'", $Advertisement.ADV_AdvertisementID)
-                            $ScheduledMessageID = Get-CimInstance @getPackageSplat @connectionSplat
+                            $getTaskSequenceSplat['Query'] = [string]::Format("SELECT ScheduledMessageID FROM CCM_Scheduler_ScheduledMessage WHERE ScheduledMessageID LIKE '{0}%'", $Advertisement.ADV_AdvertisementID)
+                            $ScheduledMessageID = switch ($Computer -eq $env:ComputerName) {
+                                $true {
+                                    Get-CimInstance @getTaskSequenceSplat @connectionSplat
+                                }
+                                $false {
+                                    Get-CCMCimInstance @getTaskSequenceSplat @connectionSplat
+                                }
+                            }
                             if ($null -ne $ScheduledMessageID) {
                                 Invoke-CCMTriggerSchedule -ScheduleID $ScheduledMessageID.ScheduledMessageID @connectionSplat
                             }
