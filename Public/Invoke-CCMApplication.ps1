45506ff32d8e0c0e7cb6ee4622f5640276d4b8cf
Cody Mathis <cody.mathis@dart.biz>
2020-03-03 07:15:50 -0500

    update help

--- Public/Invoke-CCMApplication.ps1
+++ Public/Invoke-CCMApplication.ps1
@@ -4,6 +4,15 @@ Function Invoke-CCMApplication {
             Invoke the provided method for an application deployed to a computer
         .DESCRIPTION
             Uses the Install, or Uninstall method of the CCM_Application CIMClass to perform actions on applications.
+
+            Not that you cannot inherently invoke these methods on every single application. It will have to adhere
+            to the same logic that any application must follow for installation. This includes meeting application 
+            requirements, being 'Applicable' in the sense of trying to 'Install' an application that is not currently
+            detected as installed, or trying to 'Uninstall' an application that is currently detected as installed, 
+            and it has an Uninstall command. 
+
+            The most surefire way to invoke an application method is to do so as system. Otherwise, you can also do
+            the invoking as the current interactive user of the targeted machine. 
         .PARAMETER ID
             An array of ID to invoke
         .PARAMETER IsMachineTarget
