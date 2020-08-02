e4633db8e6c1677289213d043798c9e8aa8c41d7
Cody Mathis <cody.mathis123@gmail.com>
2020-08-01 22:23:43 -0400

    Moving more things away from the base64 stuff

--- Public/Set-CCMManagementPoint.ps1
+++ Public/Set-CCMManagementPoint.ps1
@@ -6,20 +6,10 @@ function Set-CCMManagementPoint {
             This function will set the current management point for the MEMCM Client. This is done using the Microsoft.SMS.Client COM Object.
         .PARAMETER ManagementPointFQDN
             The desired management point that will be set for the specified computers/cimsessions
-        .PARAMETER CimSession
-            Provides CimSessions to set the current management point for
         .PARAMETER ComputerName
             Provides computer names to set the current management point for
         .PARAMETER PSSession
             Provides PSSession to set the current management point for
-        .PARAMETER ConnectionPreference
-            Determines if the 'Get-CCMConnection' function should check for a PSSession, or a CIMSession first when a ComputerName
-            is passed to the function. This is ultimately going to result in the function running faster. The typical use case is
-            when you are using the pipeline. In the pipeline scenario, the 'ComputerName' parameter is what is passed along the
-            pipeline. The 'Get-CCMConnection' function is used to find the available connections, falling back from the preference
-            specified in this parameter, to the the alternative (eg. you specify, PSSession, it falls back to CIMSession), and then
-            falling back to ComputerName. Keep in mind that the 'ConnectionPreference' also determines what type of connection / command
-            the ComputerName parameter is passed to.
         .EXAMPLE
             C:\PS> Set-CCMManagementPoint -ManagementPointFQDN 'cmmp1.contoso.com'
                 Sets the local computer's management point to cmmp1.contoso.com
@@ -31,24 +21,19 @@ function Set-CCMManagementPoint {
             Author:      Cody Mathis
             Contact:     @CodyMathis123
             Created:     2020-01-18
-            Updated:     2020-03-01
+            Updated:     2020-08-01
     #>
     [CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'ComputerName')]
     [Alias('Set-CCMMP')]
     param(
         [parameter(Mandatory = $true)]
         [string]$ManagementPointFQDN,
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'CimSession')]
-        [Microsoft.Management.Infrastructure.CimSession[]]$CimSession,
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ComputerName')]
         [Alias('Connection', 'PSComputerName', 'PSConnectionName', 'IPAddress', 'ServerName', 'HostName', 'DNSHostName')]
         [string[]]$ComputerName = $env:ComputerName,
         [Parameter(Mandatory = $false, ParameterSetName = 'PSSession')]
         [Alias('Session')]
-        [System.Management.Automation.Runspaces.PSSession[]]$PSSession,
-        [Parameter(Mandatory = $false, ParameterSetName = 'ComputerName')]
-        [ValidateSet('CimSession', 'PSSession')]
-        [string]$ConnectionPreference
+        [System.Management.Automation.Runspaces.PSSession[]]$PSSession
     )
     begin {
         $SetCurrentManagementPointScriptBlockString = [string]::Format('(New-Object -ComObject Microsoft.SMS.Client).SetCurrentManagementPoint("{0}", 1)', $ManagementPointFQDN)
@@ -61,11 +46,7 @@ function Set-CCMManagementPoint {
         foreach ($Connection in (Get-Variable -Name $PSCmdlet.ParameterSetName -ValueOnly)) {
             $getConnectionInfoSplat = @{
                 $PSCmdlet.ParameterSetName = $Connection
-            }
-            switch ($PSBoundParameters.ContainsKey('ConnectionPreference')) {
-                $true {
-                    $getConnectionInfoSplat['Prefer'] = $ConnectionPreference
-                }
+                Prefer                     = 'PSSession'
             }
             $ConnectionInfo = Get-CCMConnection @getConnectionInfoSplat
             $Computer = $ConnectionInfo.ComputerName
